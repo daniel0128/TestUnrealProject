2601e4c809d9827bfddc46ae5ebfa4bdae05821f
Hanzhou Shi <hanzhou87@gmail.com>
2016-04-11 23:35:56 -0700

    Adding code.

---
+++ Source/TestProject/TestProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPROJECT_API ATestProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
